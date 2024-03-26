d5be2a4ddf06c337b37c1ef7409dde8079e6152a
Luna <luna_softproducts@pm.me>
2024-03-26 17:46:07 -0300

    main menu

---
+++ src/ui/main_menu.gd
@@ -0,0 +1,16 @@
+class_name MainMenu
+extends Control
+
+@export var game_scene:PackedScene
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_quit_button_button_down():
+	get_tree().quit()
+
+
+func _on_play_button_button_down():
+	var scene = game_scene.instantiate()
+	get_tree().root.add_child(scene)
+	queue_free()
