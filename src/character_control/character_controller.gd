d342b5df0a3d3c9f356b60e6fd08d4c7f60c948e
Arthur Hoff <abithoff@gmail.com>
2024-03-26 19:11:46 -0300

    working on player controller

--- src/character_control/character_controller.gd
+++ src/character_control/character_controller.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const JOLT_FORCE = 0.75
 const MOUSE_SEN = 0.25;
 const CAMERA_DIST = 3.0;
 
@@ -9,39 +10,54 @@ var yaw = 0.0
 var pitch = 0.0
 var player_front = Vector3(0.0,0.0,-1.0);
 
+var grounded = true;
+
+var cur_force = Vector3(0.0,0.0,0.0);
+var damp_amt = 0.9;
+
 @onready var collisionShape = $CollisionShape3D;
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _process(delta):
+func process_grounded(delta):
 	player_front = Vector3(
 		cos(PI * yaw / 180) * cos(PI * pitch / 180),
 		sin(PI * pitch / 180),
 		sin(PI * yaw / 180) * cos(PI * pitch / 180)
 	).normalized();
 	$".".look_at(Vector3(global_position + player_front));
-	DebugDraw3D.draw_line(global_position + Vector3(0.0, 2.25, 0.0), global_position + Vector3(0.0, 2.25, 0.0) + player_front * 1.25, Color("#ff0066"));
 	
+func process_flying(delta):
+	player_front = Vector3(
+		cos(PI * yaw / 180) * cos(PI * pitch / 180),
+		sin(PI * pitch / 180),
+		sin(PI * yaw / 180) * cos(PI * pitch / 180)
+	).normalized();
+	$".".look_at(Vector3(global_position + player_front));
 
-func  _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+func _process(delta):
+	if(grounded):
+		process_grounded(delta);
+	else:
+		process_flying(delta);
+	DebugDraw3D.draw_arrow(global_position + Vector3(0.0, 2.25, 0.0), global_position + Vector3(0.0, 2.25, 0.0) + velocity * 0.35, Color("#ff0066"));
 	
-func _input(event):
-	if event is InputEventMouseMotion:
-		yaw += event.relative.x * MOUSE_SEN;
-		pitch += -event.relative.y * MOUSE_SEN;
-		pitch = clamp(pitch, -89, 89);
-		print_debug(yaw, " ", pitch);
-
-func _physics_process(delta):
-	# Add the gravity.
 	if !is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("player_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("player_up") and grounded:
+		if(!is_on_floor()):
+			velocity.y = -0.5;
+			cur_force += Vector3(0.0, 1.0, 0.0) * JOLT_FORCE; 
+			grounded = false;
+		else:
+			velocity.y = JUMP_VELOCITY
+	
+	# Handle ground state	
+	if(is_on_floor() and !grounded):
+		grounded = true;		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -53,5 +69,19 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	velocity += cur_force;
+	cur_force *= damp_amt;
+
+	print_debug(velocity);
 
 	move_and_slide()
+
+func  _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += event.relative.x * MOUSE_SEN;
+		pitch += -event.relative.y * MOUSE_SEN;
+		pitch = clamp(pitch, -89, 89);
+		print_debug(yaw, " ", pitch);
