6e9b901d794ca5f12ddba93d3011d1a056df1717
Luna <luna_softproducts@pm.me>
2024-04-05 16:25:24 -0300

    camera dynamic fov

--- src/character_control/character_camera.gd
+++ src/character_control/character_camera.gd
@@ -1,11 +1,21 @@
 extends Camera3D
 
+@export var default_fov: float 
+@export var max_fov: float
 @export var camera_position: Vector3 = Vector3(0, 2, 4);
 
-@onready var player: Node3D = $"..";
+@onready var player: CharacterController = $"..";
 @onready var caster: ShapeCast3D = $"../ShapeCast3D";
 
 func _process(delta):
+	collide_with_terrain(delta)
+	update_fov(delta)
+
+func update_fov(delta):
+	var target_fov = lerpf(default_fov,max_fov, player.velocity.length() / player.MAX_VELOCITY)
+	fov = lerpf(fov, target_fov, 10 * delta)
+
+func collide_with_terrain(delta):
 	var target = player.to_global(camera_position);
 	caster.target_position = caster.to_local(target);
 	caster.force_shapecast_update();
