5c6caaa12af470c3ba8b4d766e9d38d458734d9f
Hoff22 <abithoff@gmail.com>
2024-04-06 02:00:50 -0300

    executavel

--- scenes/gameStateUpdater.gd
+++ scenes/gameStateUpdater.gd
@@ -5,7 +5,7 @@ extends Node3D
 @onready var compounds = get_tree().get_nodes_in_group("Compound");
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var good = false;
+	var good = true;
 	for compound in compounds:
 		good = good && compound.complete_task;
 	if(good):
--- src/ui/end_menu.gd
+++ src/ui/end_menu.gd
@@ -1,5 +1,8 @@
 class_name EndMenu
 extends Node
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
+
 func _on_return_button_button_down():
 	get_tree().quit()
